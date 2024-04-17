0c8a7656c7aca4db96f64605b8ca2a224a12cfa8
NguyenThuc2k3 <163957765+NguyenThuc2k3@users.noreply.github.com>
2024-04-17 10:30:24 +0700

    Update MainClass.java

--- bai5/interface/src/MainClass.java
+++ bai5/interface/src/MainClass.java
@@ -1,5 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
+        // comment
 //        Person student = new Student();
 //        Person employee = new Employee();
 //
